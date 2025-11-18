925f27a049b62c117b45c141c570d7200556c007
dhrithidesai <desaidhrithi03@gmail.com>
2025-11-18 09:15:00 +0530

    day1 programs are copied

---
+++ p1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int abc() {
+    float num=4438438785.75;
+    printf("Size of Num = %d \n",sizeof(num));
+     printf("Size of Num = %d ",sizeof(4.75));
+     printf("Value of Num: %f",num);
+}
